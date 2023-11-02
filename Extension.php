18eed9a9e3504d0d8d63f1f85bcaee62f8ebef4d
Dejosel <newjos7@gmail.com>
2023-11-02 10:20:34 -0500

    Ext init

---
+++ Extension.php
@@ -0,0 +1,58 @@
+<?php namespace Dejosel\TastyAjax;
+
+use System\Classes\BaseExtension;
+
+/**
+ * TastyAjax Extension Information File
+ */
+class Extension extends BaseExtension
+{
+    /**
+     * Register method, called when the extension is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this extension.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return [
+// Remove this line and uncomment the line below to activate
+//            'Dejosel\TastyAjax\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any admin permissions used by this extension.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+// Remove this line and uncomment block to activate
+        return [
+//            'Dejosel.TastyAjax.SomePermission' => [
+//                'description' => 'Some permission',
+//                'group' => 'module',
+//            ],
+        ];
+    }
+}
