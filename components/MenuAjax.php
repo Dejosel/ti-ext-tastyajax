15912d1ea86cbc2e142d66254440f8a4b20c18bd
Dejosel <newjos7@gmail.com>
2023-11-02 11:21:04 -0500

    Add Menuajax Component

---
+++ components/MenuAjax.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dejosel\TastyAjax\Components;
+
+use System\Classes\BaseComponent;
+
+class MenuAjax extends BaseComponent
+{
+    public function defineProperties()
+    {
+        return [];
+    }
+
+    public function onRun() {
+        // Do something when the component is loaded by a page or layout
+    }
+}
